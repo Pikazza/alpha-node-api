3b5b1eab37371a35452e1af8019035e1910720fc
Pikazza <karanit91@gmail.com>
2018-03-29 01:18:14 +0530

    pikazza optimized notifier

--- Scheduler/Notifier.js
+++ Scheduler/Notifier.js
@@ -7,6 +7,10 @@ const scriptService= require('../Services/ScriptService');
 
 let task =cron.schedule('*/1 * * * *', function(){
   console.log('running a task every single minute');
+  var date = new Date();
+  var hour = d.getHours();
+  var minutes = d.getMinutes();
+
 	scriptService.dailyScripts("02.00" ,function (err , result){
 	            if (err) {
 	                next(err);
